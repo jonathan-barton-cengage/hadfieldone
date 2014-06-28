c452da614e59c6d173783da50ed444ba42276183
jonathan-barton-cengage <jonathan.barton@cengage.com>
2014-06-28 04:25:26 -0400

    web app structure

---
+++ web/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('jonathan-barton-cengage.Hadfieldone')
+
+  .controller('MainCtrl', function($scope, $location, version) {
+
+    $scope.$path = $location.path.bind($location);
+    $scope.version = version;
+
+  });
---
+++ web/app/scripts/services/sample.js
@@ -0,0 +1,3 @@
+'use strict';
+
+angular.module('jonathan-barton-cengage.Hadfieldone')
