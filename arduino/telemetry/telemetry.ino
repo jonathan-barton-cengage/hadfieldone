06d65a86b9e0464352bd9b91c7dc3e8195b249a0
jonathan-barton-cengage <jonathan.barton@cengage.com>
2014-07-20 10:51:27 -0300

    switched gps to hardware serial.rtty issues.

--- arduino/telemetry/telemetry.ino
+++ arduino/telemetry/telemetry.ino
@@ -1,15 +1,15 @@
 //includes
 #include <TinyGPS++.h>
+//#include <SoftwareSerial.h>
 #include <Wire.h>
 #include <SD.h> //SD Logger
 #include <Adafruit_Sensor.h>  //bmp180
 #include <Adafruit_BMP085_U.h>  //bmp180 (formerly 085)
-#include <SoftwareSerial.h>
+//#include <SoftwareSerial.h>
 #include <util/crc16.h>
 //pin defines
 #define LOGPIN 10 //10 is sd logger chipset for adafruit sd logger shield
 #define RADIOPIN 9
-#define BATTERY_ADC A0
 //constants
 //const int sdLoggerChipSelect = 10; //adafruit sd logger shield chipset define
 const float seaLevelPressure = SENSORS_PRESSURE_SEALEVELHPA;
@@ -21,32 +21,165 @@ static const int GPS_ARDUINO_TX = 3;
 static const uint32_t GPSBaud = 4800;
 const uint32_t serialBaud = 115200;
 const uint32_t RADIO_LOW = 100; //105
-const uint32_t RADIO_HIGH = 10; //120
+const uint32_t RADIO_HIGH = 110; //120
 //sensors
 Adafruit_BMP085_Unified bmp = Adafruit_BMP085_Unified(10085); 
 //RTC_DS1307 RTC;
 //gps
 TinyGPSPlus gps;
 //emulated serial connection to the GPS
-SoftwareSerial ss(GPS_ARDUINO_RX, GPS_ARDUINO_TX);
+//SoftwareSerial ss(GPS_ARDUINO_RX, GPS_ARDUINO_TX);
+
 //logging related
 uint16_t sentence_id = 0;
 //debug
 const int debug = 1;
 
+void rtty_txstring (char * string)
+{
+ 
+/* Simple function to sent a char at a time to
+ ** rtty_txbyte function.
+ ** NB Each char is one byte (8 Bits)
+ */
+ 
+char c;
+ 
+c = *string++;
+ 
+while ( c != '\0')
+ {
+ rtty_txbyte (c);
+ c = *string++;
+ }
+}
+void rtty_txbyte (char c)
+{
+ /* Simple function to sent each bit of a char to
+ ** rtty_txbit function.
+ ** NB The bits are sent Least Significant Bit first
+ **
+ ** All chars should be preceded with a 0 and
+ ** proceed with a 1. 0 = Start bit; 1 = Stop bit
+ **
+ */
+ 
+int i;
+ 
+rtty_txbit (0); // Start bit
+ 
+// Send bits for for char LSB first
+ 
+for (i=0;i<7;i++) // Change this here 7 or 8 for ASCII-7 / ASCII-8
+ {
+ if (c & 1) rtty_txbit(1);
+ 
+else rtty_txbit(0);
+ 
+c = c >> 1;
+ 
+}
+rtty_txbit (1); // Stop bit
+rtty_txbit (1); // Stop bit
+}
+ 
+void rtty_txbit (int bit)
+{
+ if (bit)
+ {
+ // high
+ analogWrite(RADIOPIN,110);
+ }
+ else
+ {
+ // low
+ analogWrite(RADIOPIN,100);
+ 
+}
+ 
+// delayMicroseconds(3370); // 300 baud
+ delayMicroseconds(10000); // For 50 Baud uncomment this and the line below.
+ delayMicroseconds(10150); // You can't do 20150 it just doesn't work as the
+ // largest value that will produce an accurate delay is 16383
+ // See : http://arduino.cc/en/Reference/DelayMicroseconds
+ 
+}
+ 
 uint16_t gps_CRC16_checksum (char *string)
 {
-	size_t i;
-	uint16_t crc;
-	uint8_t c;
-	crc = 0xFFFF;
-	// Calculate checksum ignoring the first two $s
-	for (i = 2; i < strlen(string); i++)
-	{
-		c = string[i];
-		crc = _crc_xmodem_update (crc, c);
-	}
-	return crc;
+ size_t i;
+ uint16_t crc;
+ uint8_t c;
+ 
+crc = 0xFFFF;
+ 
+// Calculate checksum ignoring the first two $s
+ for (i = 2; i < strlen(string); i++)
+ {
+ c = string[i];
+ crc = _crc_xmodem_update (crc, c);
+ }
+ 
+return crc;
+}
+ 
+void setPwmFrequency(int pin, int divisor) {
+ byte mode;
+ if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
+ switch(divisor) {
+ case 1:
+ mode = 0x01;
+ break;
+ case 8:
+ mode = 0x02;
+ break;
+ case 64:
+ mode = 0x03;
+ break;
+ case 256:
+ mode = 0x04;
+ break;
+ case 1024:
+ mode = 0x05;
+ break;
+ default:
+ return;
+ }
+ if(pin == 5 || pin == 6) {
+ TCCR0B = TCCR0B & 0b11111000 | mode;
+ }
+ else {
+ TCCR1B = TCCR1B & 0b11111000 | mode;
+ }
+ }
+ else if(pin == 3 || pin == 11) {
+ switch(divisor) {
+ case 1:
+ mode = 0x01;
+ break;
+ case 8:
+ mode = 0x02;
+ break;
+ case 32:
+ mode = 0x03;
+ break;
+ case 64:
+ mode = 0x04;
+ break;
+ case 128:
+ mode = 0x05;
+ break;
+ case 256:
+ mode = 0x06;
+ break;
+ case 1024:
+ mode = 0x7;
+ break;
+ default:
+ return;
+ }
+ TCCR2B = TCCR2B & 0b11111000 | mode;
+ }
 }
 
 static void smartDelay(unsigned long ms)
@@ -54,8 +187,8 @@ static void smartDelay(unsigned long ms)
   unsigned long start = millis();
   do 
   {
-    while (ss.available())
-      gps.encode(ss.read());
+    while (Serial.available())
+      gps.encode(Serial.read());
   } while (millis() - start < ms);
 }
 
@@ -97,106 +230,6 @@ char * getSensorMessage(){
   return txstring;
 }
 
-void rtty_txstring (char * string)
-{
-  char c;
-  c = *string++;  
-  while ( c != '\0') {
-     rtty_txbyte (c);
-     c = *string++;
-  }
-}
-
-void rtty_txbyte (char c) {
-  int i;
-  rtty_txbit (0); // Start bit
-  // Send bits for for char LSB first
-  for (i=0;i<7;i++) // Change this here 7 or 8 for ASCII-7 / ASCII-8
-   {
-     if (c & 1) rtty_txbit(1);
-     else rtty_txbit(0);
-     c = c >> 1;
-  }
-    rtty_txbit (1); // Stop bit
-    rtty_txbit (1); // Stop bit
-}
- 
-void rtty_txbit (int bit) {
- if (bit) {
-   // high
-   analogWrite(RADIOPIN,RADIO_HIGH);
- }
- else {
-   // low
-   analogWrite(RADIOPIN,RADIO_LOW);
-}
- 
-// delayMicroseconds(3370); // 300 baud
- delayMicroseconds(10000); // For 50 Baud uncomment this and the line below.
- delayMicroseconds(10150); // You can't do 20150 it just doesn't work as the
- // largest value that will produce an accurate delay is 16383
- // See : http://arduino.cc/en/Reference/DelayMicroseconds
- 
-}
-
-void setPwmFrequency(int pin, int divisor) {
- byte mode;
- if(pin == 5 || pin == 6 || pin == 9 || pin == 10) {
-   switch(divisor) {
-   case 1:
-     mode = 0x01;
-   break;
-   case 8:
-     mode = 0x02;
-   break;
-   case 64:
-     mode = 0x03;
-   break;
-   case 256:
-     mode = 0x04;
-   break;
-   case 1024:
-     mode = 0x05;
-   break;
-   default:
-     return;
-   }
-   if(pin == 5 || pin == 6) {
-     TCCR0B = TCCR0B & 0b11111000 | mode;
-   }
-   else {
-     TCCR1B = TCCR1B & 0b11111000 | mode;
-   }
- } else if(pin == 3 || pin == 11) {
-     switch(divisor) {
-       case 1:
-         mode = 0x01;
-       break;
-       case 8:
-         mode = 0x02;
-       break;
-       case 32:
-         mode = 0x03;
-       break;
-       case 64:
-         mode = 0x04;
-       break;
-       case 128:
-         mode = 0x05;
-       break;
-       case 256:
-         mode = 0x06;
-       break;
-       case 1024:
-         mode = 0x7;
-       break;
-       default:
-         return;
-     }
-     TCCR2B = TCCR2B & 0b11111000 | mode;
-  }
-}
-
 void safeSerialPrintLn(char * message){
   if(debug == 1){
     Serial.println(message);
@@ -205,23 +238,24 @@ void safeSerialPrintLn(char * message){
 
 void setup(void){
     //debug setup
-    
     if(debug == 1){ 
-      Serial.begin(serialBaud); 
+      //Serial.begin(serialBaud); 
     }
     
+    //gps setup
+    Serial.begin(GPSBaud);
     //sensor setup
+    
     Wire.begin();
     if(!bmp.begin()){
       safeSerialPrintLn("0. Sensor is not functioning. ");       
     }
     
-    //gps setup
-    ss.begin(GPSBaud);
     
     //radio setup
     pinMode(RADIOPIN, OUTPUT);
     setPwmFrequency(RADIOPIN, 1);
+    
     //log setup
     pinMode(LOGPIN, OUTPUT);
     if(SD.begin(LOGPIN)){
@@ -230,6 +264,7 @@ void setup(void){
       safeSerialPrintLn("1. Failed to load SD card.");
       return;
     }
+   
 }
 
 void loop(void){
@@ -237,16 +272,15 @@ void loop(void){
    char * message;
    message = getSensorMessage();
    safeSerialPrintLn(message);
-   rtty_txstring (message);
-   safeSerialPrintLn(message);
+   
    if(hadfieldDataLog){
      safeSerialPrintLn("2. Opened data log.");
      hadfieldDataLog.println(String(message));
      hadfieldDataLog.close();
    }
-   
+   smartDelay(0);
+   rtty_txstring (message);
 
-  // Dispatch incoming characters
-  
-  smartDelay(1000);
+  //delay(1000);
+  smartDelay(20000);
 }
